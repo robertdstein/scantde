381b618d1e876b804e632e46c732329a2bc039d6
Robert Stein <rdstein@umd.edu>
2025-07-21 11:04:09 -0400

    Working merge logs

--- scantde/log/merge.py
+++ scantde/log/merge.py
@@ -1,3 +1,5 @@
+import pandas as pd
+
 from scantde.log.model import ProcStage
 
 def merge_processing_logs(
@@ -9,12 +11,21 @@ def merge_processing_logs(
     :param logs: List of lists of ProcStage objects
     :return: Merged list of ProcStage objects
     """
-    # merged_log = []
-    # for log in logs:
-    #     merged_log.extend(log)
-    #
-    # # Sort by timestamp to maintain chronological order
-    # merged_log.sort(key=lambda x: x.timestamp)
-    raise NotImplementedError()
 
-    return merged_log
+    dfs = [pd.DataFrame([x.model_dump() for x in log]) for log in logs]
+    new = dfs[0]
+
+    for df in dfs[1:]:
+        new = new.merge(
+            df,
+            how="outer",
+            on=["stage"]
+        )
+        new["n_sources"] = new["n_sources_x"].fillna(0) + new["n_sources_y"].fillna(0)
+        new["tdes"] = [list(set(x["tdes_x"] + x["tdes_x"])) for _, x in new.iterrows()]
+        new.sort_values(by=["n_sources"], ascending=False, inplace=True)
+        new.drop(columns=["n_sources_x", "n_sources_y", "tdes_x", "tdes_y"], inplace=True)
+
+    merged = [ProcStage(**row) for _, row in new.iterrows()]
+
+    return merged
